49f57934d10c94619f3daf5c09868a4876aca965
Mickey <codemickeycode@gmail.com>
2013-06-19 15:01:07 +0800

    first commit

---
+++ .content/my_first_blogpost.md
@@ -0,0 +1,6 @@
+Title: hello world
+Date: 2013-05-25 18:00
+Category: code
+Tags: pelican, python
+Slug: the-way-of-the-pelican
+Author: juan tamad
---
+++ settings.py
@@ -0,0 +1,12 @@
+
+from __future__ import unicode_literals
+
+AUTHOR = 'juan tamad'
+SITENAME = 'the misadventures of juan tamad'
+SITEURL = 'http://juantamad.ph'
+DEFAULT_DATE_FORMAT = '%A, %B %d, %Y'
+
+GITHUB_URL = 'https://github.com/codemickeycode/pelican_blog'
+
+TIMEZONE = 'Asia/Manila'
+DEFAULT_PAGINATION = 1
